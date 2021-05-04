0512a2ed57f079200964057475b70259b093fc99
Victoria <victoriabotelho14@gmail.com>
2021-05-04 14:30:59 -0300

    Manifest updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Portifólio de Victoria Botelho Martins',
+        short_name: 'Portifólio de Victoria Botelho Martins',
         start_url: '/',
         background_color: '#3e58a6',
         theme_color: '#3e58a6',
