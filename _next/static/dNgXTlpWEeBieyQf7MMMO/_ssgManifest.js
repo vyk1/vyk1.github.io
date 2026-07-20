667d11e251749ffbe2809964f8fd14d9c994d6f1
vyk1 <vyk1@users.noreply.github.com>
2026-07-20 21:48:45 +0000

    deploy: b79adfc4cda13d865465fe80c7864f8077783e0f

---
+++ _next/static/dNgXTlpWEeBieyQf7MMMO/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fen\u002Fblog\u002F[slug]","\u002Fen\u002Fprojetos\u002F[slug]","\u002Fprojetos\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
